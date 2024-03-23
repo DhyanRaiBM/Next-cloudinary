db38d92219fe26d59387e66c89d59bdbf01831b6
Dhyan Rai <dhyanraibm@gmail.com>
2024-03-23 16:22:24 +0530

    My first commit

---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,7 @@
+type Props = {}
+const Page = (props: Props) => {
+  return (
+    <div>Update</div>
+  )
+}
+export default Page
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,7 @@
+type Props = {}
+const Page = (props: Props) => {
+  return (
+    <div>Add</div>
+  )
+}
+export default Page
